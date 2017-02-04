f61a7ebe1adbc4aaa0762331d32bceb694472672
Paul D Handy <paul.d.handy@gmail.com>
2017-02-04 15:34:39 -0700

    data is shit. work is not?

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import turbo from './gl2c'
 import PearlDiver from './search'
+import Curl from "iota.lib.js/lib/crypto/curl"
+import * as Converter from "iota.lib.js/lib/crypto/converter"
+
 
 {
+  var TRANSACTION_LENGTH = 8019;
+  var HASH_LENGTH = 243;
+  var STATE_LENGTH = HASH_LENGTH*3;
+  var curl =  new Curl();
+  curl.initialize(new Int32Array(STATE_LENGTH));
   var pearlDiver = new PearlDiver();
   var minWeightMagnitude = 13;
-  var testTrits = new Int32Array(8019);
+  var testTrits = new Int32Array(TRANSACTION_LENGTH);
   for(var i=0; i < testTrits.length; i++) testTrits[i] = Math.floor(Math.random() * 3) - 1;
   var start = Date.now();
   var p1 = pearlDiver.search(testTrits, minWeightMagnitude);
   p1.then((hash) => {
-    console.log("hashed mwm of " + minWeightMagnitude + " in " + (Date.now()-start)/1e3 +"s");
-    console.log(hash.slice());
+    var diff = (Date.now()-start)/1e3 ;
+    var output = [...testTrits.slice(0,TRANSACTION_LENGTH-HASH_LENGTH), ...hash];
+    var checkHash = new Int32Array(HASH_LENGTH);
+
+    console.log("hashed mwm of " + minWeightMagnitude + " in " + diff +"s. OL: " + output.length);
+
+    curl.absorb(output);
+    curl.squeeze(checkHash);
+
+    console.log(Converter.trytes(checkHash));
   }).catch((err) => {
-    console.log("error occured");
     console.log(err);
   });
 }
--- src/search.js
+++ src/search.js
@@ -1,16 +1,13 @@
 import Turbo from './gl2c'
 import searchInit, {transform} from './searchInit'
 import {k_init, headers, twistMain, barrier, twist, k_transform, increment, k_check} from './kernel'
+import * as Const from './constants'
 
-let HASH_LENGTH = 243,
-  STATE_LENGTH = HASH_LENGTH * 3,
-  TRANSACTION_LENGTH = HASH_LENGTH * 33;
 let MAXIMAGESIZE = 1e6;
 let dim = {};
 let texelSize = 4;
 
-dim.x = STATE_LENGTH+1;
-//dim.x = STATE_LENGTH;
+dim.x = Const.STATE_LENGTH+1;
 let imageSize= Math.floor(MAXIMAGESIZE / dim.x / texelSize ) * dim.x * texelSize;
 dim.y = imageSize / dim.x / texelSize ;
 
@@ -21,22 +18,23 @@ export default class {
     if(Turbo) {
       this.buf = Turbo.alloc(imageSize);
       this.turbo = new Turbo(this.buf.data.length, dim);
-      this.state = "READY";
-      this.findNonce = this._turboFindNonce;
       this.turbo.addProgram("init", k_init);
       this.turbo.addProgram("check", headers + k_check);
       this.turbo.addProgram("twist", headers + barrier + twist + twistMain);
+
+      this.findNonce = this._turboFindNonce;
+      this.state = "READY";
     }
   }
 
   search(transactionTrits, minWeightMagnitude) {
     return new Promise((res, rej) => {
-      if (transactionTrits.length != TRANSACTION_LENGTH) rej(new Error("Incorrect Transaction Length"));
-      else if(minWeightMagnitude >= HASH_LENGTH || minWeightMagnitude <= 0) rej(new Error("Incorrect Min-Weight Magnitude"));
+      if (transactionTrits.length != Const.TRANSACTION_LENGTH) rej(new Error("Incorrect Transaction Length"));
+      else if(minWeightMagnitude >= Const.HASH_LENGTH || minWeightMagnitude <= 0) rej(new Error("Incorrect Min-Weight Magnitude"));
       else {
         var states = {
-          low : new Int32Array(STATE_LENGTH),
-          high : new Int32Array(STATE_LENGTH)
+          low : new Int32Array(Const.STATE_LENGTH),
+          high : new Int32Array(Const.STATE_LENGTH)
         };
         searchInit(states, transactionTrits);
         this.findNonce(states, minWeightMagnitude, (hash) => res(hash));
@@ -57,30 +55,28 @@ export default class {
 
   _turboSearch(callback) {
     for(var i = 0; i < dim.y; i++) {
-      this._turboIncrement(HASH_LENGTH /3 * 2, HASH_LENGTH, i);
+      this._turboIncrement(Const.HASH_LENGTH /3 * 2, Const.HASH_LENGTH, i);
     }
     this._turboTransform();
     var {index, nonce} = this._turboCheck();
     if(index === -1) {
       requestAnimationFrame(() => {this._turboSearch(callback)});
     } else {
-      console.log("wahoo");
       var length = dim.x*texelSize;
       var start = length*index;
-      var end = start + HASH_LENGTH*texelSize;
-      console.log(index);
-      console.log("nonce: " + nonce);
+      var end = start + Const.HASH_LENGTH*texelSize;
+      console.log("index: " + index + " nonce: " + nonce);
       callback(
         this.buf.data
         .reduce(pack(4), []).reduce(pack(dim.x), [])[index]
-        .slice(0,HASH_LENGTH)
+        .slice(0,Const.HASH_LENGTH)
         .map( x => (x[0] & nonce == 0) ? 1 : ( (x[1] & nonce) == 0 ? -1 : 0))
       );
     }
   }
 
   _turboCheck() {
-    var length = (STATE_LENGTH + 1) * texelSize ;
+    var length = (Const.STATE_LENGTH + 1) * texelSize ;
     var index = -1, nonce;
     this.buf.data[length-1] = this.mwm;
     //this.turbo.run(this.buf, dim, headers + k_check);
@@ -100,8 +96,8 @@ export default class {
     var lastMeasurement = 0xFFFFFFFF;
     for (var i = this.mwm; i-- > 0; ) {
       lastMeasurement &= ~(
-        this.buf.data[len*y + (HASH_LENGTH - 1 - i) * texelSize + 2] ^
-        this.buf.data[len*y + (HASH_LENGTH - 1 - i) * texelSize + 3]);
+        this.buf.data[len*y + (Const.HASH_LENGTH - 1 - i) * texelSize + 2] ^
+        this.buf.data[len*y + (Const.HASH_LENGTH - 1 - i) * texelSize + 3]);
       if (lastMeasurement == 0) {
         return 0;
       }
@@ -113,7 +109,7 @@ export default class {
     for(var i = 1; i < dim.y; i++) {
       //console.log("offset at: " +i);
       for(var j = i; j < dim.y; j++) {
-        this._turboIncrement(HASH_LENGTH/3, (HASH_LENGTH / 3)* 2, j);
+        this._turboIncrement(Const.HASH_LENGTH/3, (Const.HASH_LENGTH / 3)* 2, j);
       }
     }
   }
@@ -144,7 +140,7 @@ export default class {
   }
 
   _turboWriteBuffers(states) {
-    for(var i = 0; i < STATE_LENGTH; i++) {
+    for(var i = 0; i < Const.STATE_LENGTH; i++) {
       this.buf.data[i * texelSize] = states.low[i];
       this.buf.data[i * texelSize + 1] = states.high[i];
       this.buf.data[i * texelSize + 2] = states.low[i];
@@ -164,5 +160,5 @@ export default class {
     //this.turbo.run(this.buf, dim, k_init, false);
     //return this._slowCheck() === 0;
     /*
-    return this.buf.data[(1 + STATE_LENGTH) * texelSize] == 0;
+    return this.buf.data[(1 + Const.STATE_LENGTH) * texelSize] == 0;
     */
